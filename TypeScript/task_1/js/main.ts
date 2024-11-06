4eb05a9c9d1b10c6d2dd715a511a498b0165a99a
Mxn-ptr <maxence0@hotmail.fr>
2024-11-06 17:51:12 +0100

    Remove useless files

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,6 +1,6 @@
 interface Teacher {
-	firstName: string;
-	lastName: string;
+	readonly firstName: string;
+	readonly lastName: string;
 	fullTimeEmployee: boolean;
 	yearsOfExperience?: number;
 	location: string;
@@ -14,5 +14,5 @@ const teacher3: Teacher = {
 	location: 'London',
 	contract: false,
 };
-  
+
 console.log(teacher3);
