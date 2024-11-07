64ffbca53952ce28f31c5d9e31f3df9c433e30ba
Mxn-ptr <maxence0@hotmail.fr>
2024-11-07 14:56:30 +0100

    Add task 3 and 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -16,3 +16,51 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+interface Directors extends Teacher {
+	numberOfReports: number;
+}
+
+const director1: Directors = {
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+  
+console.log(director1);
+
+interface printTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+	return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher(teacher3.firstName, teacher3.lastName));
+
+interface StudentClassConstructor {
+	new (firstName: string, lastName: string): IStudentClass;
+}
+  
+interface IStudentClass {
+workOnHomework(): string;
+displayName(): string;
+}
+  
+class StudentClass implements IStudentClass {
+	constructor(private firstName: string, private lastName: string) {}
+
+	workOnHomework() {
+		return 'Currently working';
+	}
+
+	displayName() {
+		return this.firstName;
+	}
+}
+
+const student = new StudentClass("John", "Doe");
+console.log(student.displayName());
